ad570bd027fc09fa333deee33cd07eeb03264619
AlejandroVillarraga <danilo.villarraga@mail.escuelaing.edu.co>
2018-02-10 10:00:13 -0500

    añadi configuracion de seguridad

--- angular-seed/src/app/app.module.ts
+++ angular-seed/src/app/app.module.ts
@@ -10,24 +10,49 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AppConfiguration } from './common/config/app-configuration.service';
+import { INITIAL_CONFIG } from './common/config/initial-config';
+import { AppDataService } from './common/app-data.service';
+import { AuthService } from './common/auth.service';
+import { APIService } from './common/api.service';
+import { HttpModule } from '@angular/http';
+import { SignInPageComponent } from './pages/sign-in/sign-in-page.component';
+import { UsersService } from './services/users.service';
+
+
 const ROUTES = [
-  { path: '', component: HomePageComponent },
-  { path: '**', component: PageNotFoundComponent }
+{ path: '', component: SignInPageComponent },
+{ path: 'home', component: HomePageComponent, canActivate: [AuthService],},
+{path: '**', component: PageNotFoundComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    SignInPageComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     RouterModule.forRoot(ROUTES),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: INITIAL_CONFIG,
+      useValue: {
+        apiURL: 'http://localhost:8080'
+      }
+    },
+    AppConfiguration,
+    AppDataService,
+    AuthService,
+    APIService,
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
