80fa1ac43211a1bda99b80ec4057039bd00d4e0a
AlejandroVillarraga <danilo.villarraga@mail.escuelaing.edu.co>
2018-02-23 19:12:51 -0500

    vistas

--- angular-seed/src/app/pages/sign-in/sign-in-page.component.ts
+++ angular-seed/src/app/pages/sign-in/sign-in-page.component.ts
@@ -33,7 +33,7 @@ export class SignInPageComponent implements OnInit {
     this.usersService.login(
       this.signInForm.get('username').value,
       this.signInForm.get('password').value).subscribe(loginResponse => {
-        this.router.navigate(['nursingservices']);
+        this.router.navigate(['home']);
       }, error => {
         this.loginError = 'Error Signing in: ' + (error && error.message ? error.message : '');
       })
