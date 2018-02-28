cf6dcd1a5302bcfc34d5936f61fd1aa6cd8c077c
Rafael <rafael.jimenez@mail.escuelaing.edu.co>
2018-02-28 11:00:25 -0500

    Modelo paciente

---
+++ angular-seed/src/app/models/Patient.ts
@@ -0,0 +1,25 @@
+export class Patient {
+
+    private username: string;
+private firstname: string;
+private lastname: string;
+private image: string;
+private email: string;
+private password:string;
+private age: number;
+private creditCardNumber: number;
+private creditCVV: number;
+
+constructor(username:string,firstname: string, lastname: string, image: string, email:string,password:string, age:number, creditCardNumber:number, creditCVV: number) {
+        this.username=username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.image = image;
+        this.email=email;
+        this.password=password;
+        this.age=age;
+        this.creditCardNumber=creditCardNumber;
+        this.creditCVV=creditCVV;
+
+        }
+}
